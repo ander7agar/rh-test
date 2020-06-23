e8d78b76fa8235c262758814439dcc2786217b00
ander7agar <ander7agar@gmail.com>
2020-06-23 15:42:09 +0200

    Added envs options

--- index.js
+++ index.js
@@ -4,3 +4,37 @@ require('dotenv').config();
 let Hapi = require('@hapi/hapi');
 let mongoose = require('mongoose');
 let RestHapi = require('rest-hapi');
+
+async function api(){
+
+    let port = process.env.SERVER_PORT || 8080;
+
+    try {
+        let server = Hapi.Server({ port: port });
+
+        let config = {
+            appTitle: "My API",
+            mongo: {
+                URI: process.env.MONGODB_URI
+            }
+        };
+
+        await server.register({
+            plugin: RestHapi,
+            options: {
+                mongoose,
+                config
+            }
+        });
+
+        await server.start();
+
+        console.log("Server ready", server.info);
+
+        return server
+    } catch (err) {
+        console.log("Error starting server:", err);
+    }
+}
+
+module.exports = api();
